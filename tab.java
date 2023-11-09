041221c3c16eafd833a5b599751b59fca2f5d9db
Théo Dubernet <95922914+ErmitePanda@users.noreply.github.com>
2023-11-09 09:44:30 +0100

    Exercice sur les tableaux terminer

--- tab.java
+++ tab.java
@@ -2,10 +2,15 @@ public class tab {
     public static void main(String[] args) {
         /* Teste nbOccAux */
         int []t = {1,2,6,8,7,7,7,7,3,-2,1};
+        int []tvide = {};
 
-        System.out.println(nbOccAux(7,t,0)); // 4
-        System.out.println(nbOccAux(-22,t,0)); // 0
-        System.out.println(nbOccAux(7,t,5)); // 3
+        //System.out.println(nbOccAux(7,t,0)); // 4
+        //System.out.println(nbOccAux(-22,t,0)); // 0
+        //System.out.println(nbOccAux(7,t,5)); // 3
+
+        System.out.println(nbOccAux2(7,t,0)); // 4
+        System.out.println(nbOccAux2(7,tvide,0)); // 0
+        System.out.println(nbOccAux2(7,t,5)); // 3
     }
 
     // Cas de base
@@ -19,6 +24,25 @@ public class tab {
 
         else {
             int tmp = nbOccAux(x, t, i+1);
+            if (t[i] == x) {
+                return 1 + tmp;
+            } else {
+                return tmp;
+            }
+        }
+    }
+
+    public static int nbOcc(int x, int []t) {
+        return nbOccAux(x,t,0);
+    }
+
+    public static int nbOccAux2(int x, int []t, int i) {
+        if (t.length == 0 || i >= t.length) {
+            return 0;
+        }
+
+        else {
+            int tmp = nbOccAux2(x,t,i+1);
             if (t[i] == x) {
                 return 1 + tmp;
             } else {
